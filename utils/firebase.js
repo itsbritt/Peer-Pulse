e315f3a8e7417faeeef0805e6b7681f1da3aa860
Stephanie Gage <stephig85@gmail.com>
2017-01-14 17:52:12 -0600

    added firebase.js and utils directory

---
+++ utils/firebase.js
@@ -0,0 +1,34 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+};
+
+// Firebase instance
+const firebaseApp = firebase.initializeApp(config);
+
+// Firebase doesn't return data as an array but as a parent object
+//  containing child objects. This will extract them into an array
+const firebaseListToArray = (firebaseObjectList) => {
+  if (!firebaseObjectList) return [];
+
+  return Object.keys(firebaseObjectList)
+    .map(k => {
+      const obj = {
+        id: k
+      };
+      for (let key in firebaseObjectList[k]) {
+        if (firebaseObjectList[k].hasOwnProperty(key)) {
+          obj[key] = firebaseObjectList[k][key];
+        }
+      }
+      return obj;
+    });
+}
+
+export { firebaseApp as firebase };
+export { firebaseListToArray };
